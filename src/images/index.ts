9dab29a3ae58615e3b4f4c03842e410aae1431f6
이지우 <jiwoo.lee@trialinformatics.com>
2025-11-02 21:03:13 +0900

    publish

--- src/images/index.ts
+++ src/images/index.ts
@@ -21,30 +21,51 @@ import image20 from "./image (20).jpg"
 import image21 from "./image (21).jpg"
 import image22 from "./image (22).jpg"
 import image23 from "./image (23).jpg"
+import image24 from "./image (24).jpg"
+import image25 from "./image (25).jpg"
+import image26 from "./image (26).jpg"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_IMAGES = [
-  { thumb: image2, original: image2 },
-  { thumb: image3, original: image3 },
-  { thumb: image4, original: image4 },
+  { thumb: image19, original: image19 },
   { thumb: image5, original: image5 },
   { thumb: image6, original: image6 },
   { thumb: image7, original: image7 },
   { thumb: image8, original: image8 },
+  { thumb: image24, original: image24 },
   { thumb: image9, original: image9 },
   { thumb: image10, original: image10 },
   { thumb: image11, original: image11 },
+  { thumb: image23, original: image23 },
+  { thumb: image26, original: image26 },
+  { thumb: image4, original: image4 },
+  { thumb: image2, original: image2 },
+  { thumb: image3, original: image3 },
+
+
+
+
+
+  { thumb: image21, original: image21 },
+  { thumb: image16, original: image16 },
+  { thumb: image25, original: image25 },
   { thumb: image12, original: image12 },
   { thumb: image13, original: image13 },
   { thumb: image14, original: image14 },
   { thumb: image15, original: image15 },
-  { thumb: image16, original: image16 },
+  { thumb: image20, original: image20 },
+
+
+
+
   { thumb: image17, original: image17 },
   { thumb: image18, original: image18 },
-  { thumb: image19, original: image19 },
-  { thumb: image20, original: image20 },
-  { thumb: image21, original: image21 },
+
+
   { thumb: image22, original: image22 },
-  { thumb: image23, original: image23 },
+
+
+
+
 ]
