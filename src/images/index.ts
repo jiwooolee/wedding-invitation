3e6beea8b9e1f3bd1660b681dbacd02f75283c73
이지우 <jiwoo.lee@trialinformatics.com>
2025-11-16 18:51:25 +0900

    publish

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,56 +1,82 @@
 import coverImage from "./image (1).jpg"
-import image2 from "./image (2).jpg"
-import image3 from "./image (3).jpg"
-import image4 from "./image (4).jpg"
-import image5 from "./image (5).jpg"
-import image6 from "./image (6).jpg"
-import image7 from "./image (7).jpg"
-import image8 from "./image (8).jpg"
-import image9 from "./image (9).jpg"
-import image10 from "./image (10).jpg"
-import image11 from "./image (11).jpg"
-import image12 from "./image (12).jpg"
-import image13 from "./image (13).jpg"
-import image14 from "./image (14).jpg"
-import image15 from "./image (15).jpg"
-import image16 from "./image (16).jpg"
-import image17 from "./image (17).jpg"
-import image18 from "./image (18).jpg"
-import image19 from "./image (19).jpg"
-import image20 from "./image (20).jpg"
-import image21 from "./image (21).jpg"
-import image22 from "./image (22).jpg"
-import image23 from "./image (23).jpg"
-import image24 from "./image (24).jpg"
-import image25 from "./image (25).jpg"
-import image26 from "./image (26).jpg"
+import originalImage2 from "./image (2).jpg"
+import originalImage3 from "./image (3).jpg"
+import originalImage4 from "./image (4).jpg"
+import originalImage5 from "./image (5).jpg"
+import originalImage6 from "./image (6).jpg"
+import originalImage7 from "./image (7).jpg"
+import originalImage8 from "./image (8).jpg"
+import originalImage9 from "./image (9).jpg"
+import originalImage10 from "./image (10).jpg"
+import originalImage11 from "./image (11).jpg"
+import originalImage12 from "./image (12).jpg"
+import originalImage13 from "./image (13).jpg"
+import originalImage14 from "./image (14).jpg"
+import originalImage15 from "./image (15).jpg"
+import originalImage16 from "./image (16).jpg"
+import originalImage17 from "./image (17).jpg"
+import originalImage18 from "./image (18).jpg"
+import originalImage19 from "./image (19).jpg"
+import originalImage20 from "./image (20).jpg"
+import originalImage21 from "./image (21).jpg"
+import originalImage22 from "./image (22).jpg"
+import originalImage23 from "./image (23).jpg"
+import originalImage24 from "./image (24).jpg"
+import originalImage25 from "./image (25).jpg"
+import originalImage26 from "./image (26).jpg"
+
+import thumbImage2 from "../thumbnail/image (2).jpg"
+import thumbImage3 from "../thumbnail/image (3).jpg"
+import thumbImage4 from "../thumbnail/image (4).jpg"
+import thumbImage5 from "../thumbnail/image (5).jpg"
+import thumbImage6 from "../thumbnail/image (6).jpg"
+import thumbImage7 from "../thumbnail/image (7).jpg"
+import thumbImage8 from "../thumbnail/image (8).jpg"
+import thumbImage9 from "../thumbnail/image (9).jpg"
+import thumbImage10 from "../thumbnail/image (10).jpg"
+import thumbImage11 from "../thumbnail/image (11).jpg"
+import thumbImage12 from "../thumbnail/image (12).jpg"
+import thumbImage13 from "../thumbnail/image (13).jpg"
+import thumbImage14 from "../thumbnail/image (14).jpg"
+import thumbImage15 from "../thumbnail/image (15).jpg"
+import thumbImage16 from "../thumbnail/image (16).jpg"
+import thumbImage17 from "../thumbnail/image (17).jpg"
+import thumbImage18 from "../thumbnail/image (18).jpg"
+import thumbImage19 from "../thumbnail/image (19).jpg"
+import thumbImage20 from "../thumbnail/image (20).jpg"
+import thumbImage21 from "../thumbnail/image (21).jpg"
+import thumbImage22 from "../thumbnail/image (22).jpg"
+import thumbImage23 from "../thumbnail/image (23).jpg"
+import thumbImage24 from "../thumbnail/image (24).jpg"
+import thumbImage25 from "../thumbnail/image (25).jpg"
+import thumbImage26 from "../thumbnail/image (26).jpg"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_IMAGES = [
-  { thumb: image5, original: image5 },
-  { thumb: image6, original: image6 },
-  { thumb: image7, original: image7 },
-  { thumb: image8, original: image8 },
-  { thumb: image24, original: image24 },
-  { thumb: image9, original: image9 },
-  { thumb: image10, original: image10 },
-  { thumb: image11, original: image11 },
-  { thumb: image23, original: image23 },
-  { thumb: image26, original: image26 },
-  { thumb: image4, original: image4 },
-  { thumb: image2, original: image2 },
-  { thumb: image3, original: image3 },
-  { thumb: image19, original: image19 },
-  { thumb: image21, original: image21 },
-  { thumb: image16, original: image16 },
-  { thumb: image25, original: image25 },
-  { thumb: image12, original: image12 },
-  { thumb: image13, original: image13 },
-  { thumb: image14, original: image14 },
-  { thumb: image15, original: image15 },
-  { thumb: image20, original: image20 },
-  { thumb: image17, original: image17 },
-  { thumb: image18, original: image18 },
-  { thumb: image22, original: image22 },
+  { thumb: thumbImage5, original: originalImage5 },
+  { thumb: thumbImage6, original: originalImage6 },
+  { thumb: thumbImage7, original: originalImage7 },
+  { thumb: thumbImage8, original: originalImage8 },
+  { thumb: thumbImage24, original: originalImage24 },
+  { thumb: thumbImage9, original: originalImage9 },
+  { thumb: thumbImage10, original: originalImage10 },
+  { thumb: thumbImage11, original: originalImage11 },
+  { thumb: thumbImage23, original: originalImage23 },
+  { thumb: thumbImage26, original: originalImage26 },
+  { thumb: thumbImage4, original: originalImage4 },
+  { thumb: thumbImage2, original: originalImage2 },
+  { thumb: thumbImage3, original: originalImage3 },
+  { thumb: thumbImage19, original: originalImage19 },
+  { thumb: thumbImage21, original: originalImage21 },
+  { thumb: thumbImage16, original: originalImage16 },
+  { thumb: thumbImage25, original: originalImage25 },
+  { thumb: thumbImage12, original: originalImage12 },
+  { thumb: thumbImage13, original: originalImage13 },
+  { thumb: thumbImage14, original: originalImage14 },
+  { thumb: thumbImage15, original: originalImage15 },
+  { thumb: thumbImage20, original: originalImage20 },
+  { thumb: thumbImage17, original: originalImage17 },
+  { thumb: thumbImage18, original: originalImage18 },
+  { thumb: thumbImage22, original: originalImage22 },
 ]
