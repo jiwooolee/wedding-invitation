92cf72d14c7fae210920fd87294556c8627f46ca
이지우 <jiwoo.lee@trialinformatics.com>
2026-01-09 16:57:24 +0900

    publish

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -50,6 +50,17 @@ export const Location = () => {
         </div>
         <div className="location-info">
           <div className="transportation-icon-wrapper">
+            <BusIcon className="transportation-icon" />
+          </div>
+          <div className="heading" style={{fontSize: '1rem'}}>셔틀버스</div>
+          <div />
+          <div className="content" style={{fontSize: '0.9rem'}}>
+            <p style={{margin: 0}}>강원 횡성군 안흥면 안흥리 284-11 면사무소 앞</p>
+            <p style={{margin: 0}}>오전 10시 40분</p>
+          </div>
+        </div>
+        <div className="location-info">
+          <div className="transportation-icon-wrapper">
             <ParkingIcon className="transportation-icon" />
           </div>
           <div className="heading" style={{fontSize: '1rem'}}>주차</div>
